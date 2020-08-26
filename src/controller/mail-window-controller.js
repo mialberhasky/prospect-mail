2d9688c21ff5b91fa32d70b53c7a1dc55002f06a
Michael Alberhasky <michael-alberhasky@uiowa.edu>
2020-08-26 16:42:19 -0500

    Adding spellchecking

--- src/controller/mail-window-controller.js
+++ src/controller/mail-window-controller.js
@@ -26,7 +26,11 @@ class MailWindowController {
             autoHideMenuBar: true,
             show: false,
             title: 'Prospect Mail',
-            icon: path.join(__dirname, '../../assets/outlook_linux_black.png')
+            icon: path.join(__dirname, '../../assets/outlook_linux_black.png'),
+            webPreferences: {
+                nodeIntegration: false,
+                preload: path.join(__dirname, '../js/preload.js')
+            }
         })
 
         // and load the index.html of the app.
